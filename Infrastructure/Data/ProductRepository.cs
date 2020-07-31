203dcb7dd60a099868454638a3fac3e1b0e5b05d
ojambo <ojambo04@gmail.com>
2020-07-31 18:20:23 +0530

    Add project files.

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        Task<Product> IProductRepository.GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IReadOnlyList<Product>> IProductRepository.GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
